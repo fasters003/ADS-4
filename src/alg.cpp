a136098c299ea1221a6f43e0cb7a20630df0fb7d
fasters003 <andrey1990ver@gmail.com>
2026-04-04 14:23:23 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,4 +1,5 @@
 // Copyright 2021 NNTU-CS
+
 int countPairs1(int *arr, int len, int value) {
   int count = 0;
   for (int i = 0; i < len; ++i) {
@@ -18,19 +19,25 @@ int countPairs2(int *arr, int len, int value) {
   while (left < right) {
     int sum = arr[left] + arr[right];
     if (sum == value) {
-      int leftCount = 1;
-      int rightCount = 1;
-      while (left + 1 < right && arr[left] == arr[left + 1]) {
-        ++leftCount;
+      if (arr[left] == arr[right]) {
+        int n = right - left + 1;
+        count += n * (n - 1) / 2;
+        break;
+      } else {
+        int leftCount = 1;
+        int rightCount = 1;
+        while (left + 1 < right && arr[left] == arr[left + 1]) {
+          ++leftCount;
+          ++left;
+        }
+        while (right - 1 > left && arr[right] == arr[right - 1]) {
+          ++rightCount;
+          --right;
+        }
+        count += leftCount * rightCount;
         ++left;
-      }
-      while (right - 1 > left && arr[right] == arr[right - 1]) {
-        ++rightCount;
         --right;
       }
-      count += leftCount * rightCount;
-      ++left;
-      --right;
     } else if (sum < value) {
       ++left;
     } else {
