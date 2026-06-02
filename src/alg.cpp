e8e8939ed95e70c091dc93cfd0ddbb17df11826e
fasters003 <andrey1990ver@gmail.com>
2026-06-02 13:50:10 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -50,24 +50,29 @@ int countPairs3(int *arr, int len, int value) {
     int target = value - arr[i];
     int left = i + 1;
     int right = len - 1;
+    int found = 0;
     while (left <= right) {
       int mid = left + (right - left) / 2;
       if (arr[mid] == target) {
-        int j = mid;
-        while (j >= i + 1 && arr[j] == target) {
-          count++;
-          j--;
-        }
-        j = mid + 1;
-        while (j < len && arr[j] == target) {
-          count++;
-          j++;
-        }
+        found = 1;
+        int l = mid;
+        int r = mid;
+        while (l > i && arr[l - 1] == target) l--;
+        while (r < len - 1 && arr[r + 1] == target) r++;
+        count += r - l + 1;
         break;
       } else if (arr[mid] < target) {
         left = mid + 1;
       } else {
         right = mid - 1;
+      }
+    }
+    if (!found) {
+      left = i + 1;
+      right = len - 1;
+      while (left <= right) {
+        int mid = left + (right - left) / 2;
+        left++;
       }
     }
   }
