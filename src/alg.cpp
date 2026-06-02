ec4f32db7312c8a966d8825842cead7d1c244d72
fasters003 <andrey1990ver@gmail.com>
2026-06-02 14:00:02 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -16,7 +16,7 @@ int countPairs2(int *arr, int len, int value) {
   int count = 0;
   int left = 0;
   int right = len - 1;
-  
+
   while (left < right) {
     int sum = arr[left] + arr[right];
     if (sum == value) {
@@ -25,20 +25,23 @@ int countPairs2(int *arr, int len, int value) {
         count += n * (n - 1) / 2;
         break;
       }
-      
-      int cntL = 1;
-      int cntR = 1;
-      
-      while (left + cntL < right && arr[left] == arr[left + cntL]) {
+
+      int leftVal = arr[left];
+      int rightVal = arr[right];
+      int cntL = 0;
+      int cntR = 0;
+
+      while (left < len && arr[left] == leftVal) {
         cntL++;
+        left++;
       }
-      while (right - cntR > left && arr[right] == arr[right - cntR]) {
+
+      while (right >= 0 && arr[right] == rightVal) {
         cntR++;
+        right--;
       }
-      
+
       count += cntL * cntR;
-      left += cntL;
-      right -= cntR;
     } else if (sum < value) {
       left++;
     } else {
@@ -50,13 +53,13 @@ int countPairs2(int *arr, int len, int value) {
 
 int countPairs3(int *arr, int len, int value) {
   int count = 0;
-  
+
   for (int i = 0; i < len; i++) {
     int target = value - arr[i];
     int left = i + 1;
     int right = len - 1;
     int first = -1;
-    
+
     while (left <= right) {
       int mid = left + (right - left) / 2;
       if (arr[mid] == target) {
@@ -68,12 +71,12 @@ int countPairs3(int *arr, int len, int value) {
         right = mid - 1;
       }
     }
-    
+
     if (first != -1) {
       left = first;
       right = len - 1;
       int last = first;
-      
+
       while (left <= right) {
         int mid = left + (right - left) / 2;
         if (arr[mid] == target) {
@@ -85,7 +88,7 @@ int countPairs3(int *arr, int len, int value) {
           right = mid - 1;
         }
       }
-      
+
       count += (last - first + 1);
     }
   }
