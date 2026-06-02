a786664bc0b992bd0c0360f56e68048b995926b5
fasters003 <andrey1990ver@gmail.com>
2026-06-02 15:16:34 +0300

    awa

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,11 +1,11 @@
 // Copyright 2021 NNTU-CS
 
 int searchLeft(int *arr, int left, int right, int target) {
-  int result = -1;
+  int res = -1;
   while (left <= right) {
     int mid = left + (right - left) / 2;
     if (arr[mid] == target) {
-      result = mid;
+      res = mid;
       right = mid - 1;
     } else if (arr[mid] < target) {
       left = mid + 1;
@@ -13,15 +13,15 @@ int searchLeft(int *arr, int left, int right, int target) {
       right = mid - 1;
     }
   }
-  return result;
+  return res;
 }
 
 int searchRight(int *arr, int left, int right, int target) {
-  int result = -1;
+  int res = -1;
   while (left <= right) {
     int mid = left + (right - left) / 2;
     if (arr[mid] == target) {
-      result = mid;
+      res = mid;
       left = mid + 1;
     } else if (arr[mid] < target) {
       left = mid + 1;
@@ -29,14 +29,14 @@ int searchRight(int *arr, int left, int right, int target) {
       right = mid - 1;
     }
   }
-  return result;
+  return res;
 }
 
 int countPairs1(int *arr, int len, int value) {
   int count = 0;
-  for (int i = 0; i < len - 1; i++) {
-    for (int j = i + 1; j < len; j++) {
-      if (arr[i] + arr[j] == value) {
+  for (int i = 0; i < len; i++) {
+    for (int j = 0; j < len; j++) {
+      if (i < j && arr[i] + arr[j] == value) {
         count++;
       }
     }
@@ -46,13 +46,22 @@ int countPairs1(int *arr, int len, int value) {
 
 int countPairs2(int *arr, int len, int value) {
   int count = 0;
-  for (int i = 0; i < len - 1; i++) {
-    for (int j = len - 1; j > i; j--) {
-      if (arr[i] + arr[j] == value) {
+  int left = 0;
+  int right = len - 1;
+  while (left < right) {
+    int sum = arr[left] + arr[right];
+    if (sum == value) {
+      count++;
+      int r = right - 1;
+      while (r > left && arr[left] + arr[r] == value) {
         count++;
-      } else if (arr[i] + arr[j] < value) {
-        break;
+        r--;
       }
+      left++;
+    } else if (sum < value) {
+      left++;
+    } else {
+      right--;
     }
   }
   return count;
