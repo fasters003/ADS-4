939a1a88c25fae550df3b7aec984900bc8b188b5
fasters003 <andrey1990ver@gmail.com>
2026-06-02 13:57:21 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -16,6 +16,7 @@ int countPairs2(int *arr, int len, int value) {
   int count = 0;
   int left = 0;
   int right = len - 1;
+  
   while (left < right) {
     int sum = arr[left] + arr[right];
     if (sum == value) {
@@ -24,14 +25,17 @@ int countPairs2(int *arr, int len, int value) {
         count += n * (n - 1) / 2;
         break;
       }
+      
       int cntL = 1;
       int cntR = 1;
+      
       while (left + cntL < right && arr[left] == arr[left + cntL]) {
         cntL++;
       }
       while (right - cntR > left && arr[right] == arr[right - cntR]) {
         cntR++;
       }
+      
       count += cntL * cntR;
       left += cntL;
       right -= cntR;
@@ -46,28 +50,43 @@ int countPairs2(int *arr, int len, int value) {
 
 int countPairs3(int *arr, int len, int value) {
   int count = 0;
+  
   for (int i = 0; i < len; i++) {
     int target = value - arr[i];
     int left = i + 1;
     int right = len - 1;
+    int first = -1;
+    
     while (left <= right) {
       int mid = left + (right - left) / 2;
       if (arr[mid] == target) {
-        int l = mid;
-        while (l > i && arr[l - 1] == target) {
-          l--;
-        }
-        int r = mid;
-        while (r < len - 1 && arr[r + 1] == target) {
-          r++;
-        }
-        count += r - l + 1;
-        break;
+        first = mid;
+        right = mid - 1;
       } else if (arr[mid] < target) {
         left = mid + 1;
       } else {
         right = mid - 1;
       }
+    }
+    
+    if (first != -1) {
+      left = first;
+      right = len - 1;
+      int last = first;
+      
+      while (left <= right) {
+        int mid = left + (right - left) / 2;
+        if (arr[mid] == target) {
+          last = mid;
+          left = mid + 1;
+        } else if (arr[mid] < target) {
+          left = mid + 1;
+        } else {
+          right = mid - 1;
+        }
+      }
+      
+      count += (last - first + 1);
     }
   }
   return count;
