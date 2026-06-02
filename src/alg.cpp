0ee8f27598569f3d994c295737bef9c6d0f14ad8
fasters003 <andrey1990ver@gmail.com>
2026-06-02 13:18:25 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -20,9 +20,23 @@ int countPairs2(int *arr, int len, int value) {
   while (left < right) {
     int sum = arr[left] + arr[right];
     if (sum == value) {
-      count++;
-      left++;
-      right--;
+      if (arr[left] == arr[right]) {
+        int k = right - left + 1;
+        count += (k * (k - 1)) / 2;
+        break;
+      } else {
+        int leftDup = 1;
+        int rightDup = 1;
+        while (left + leftDup < right && arr[left] == arr[left + leftDup]) {
+          leftDup++;
+        }
+        while (right - rightDup > left && arr[right] == arr[right - rightDup]) {
+          rightDup++;
+        }
+        count += leftDup * rightDup;
+        left += leftDup;
+        right -= rightDup;
+      }
     } else if (sum < value) {
       left++;
     } else {
@@ -36,13 +50,20 @@ int countPairs3(int *arr, int len, int value) {
   int count = 0;
   for (int i = 0; i < len - 1; i++) {
     int target = value - arr[i];
-    // Бинарный поиск второго элемента
     int left = i + 1;
     int right = len - 1;
     while (left <= right) {
       int mid = left + (right - left) / 2;
       if (arr[mid] == target) {
-        count++;
+        int first = mid;
+        while (first > i && arr[first - 1] == target) {
+          first--;
+        }
+        int last = mid;
+        while (last < len - 1 && arr[last + 1] == target) {
+          last++;
+        }
+        count += (last - first + 1);
         break;
       } else if (arr[mid] < target) {
         left = mid + 1;
