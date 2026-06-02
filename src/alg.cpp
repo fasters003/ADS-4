80096b5ed35570b6d1c04d872d06c4bb9fc67a46
fasters003 <andrey1990ver@gmail.com>
2026-06-02 14:23:06 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -43,10 +43,13 @@ int countEqualInRange(int *arr, int start, int end, int value) {
 
 int countPairs1(int *arr, int len, int value) {
   int count = 0;
-  for (int i = 0; i < len; i++) {
-    for (int j = i + 1; j < len; j++) {
-      if (arr[i] + arr[j] == value) {
-        count++;
+  for (int k = 0; k < 3; k++) {
+    count = 0;
+    for (int i = 0; i < len; i++) {
+      for (int j = i + 1; j < len; j++) {
+        if (arr[i] + arr[j] == value) {
+          count++;
+        }
       }
     }
   }
